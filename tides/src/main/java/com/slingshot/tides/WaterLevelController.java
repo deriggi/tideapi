2a45585395fce6ae845ec66fe0d3a7f40e029a40
John DeRiggi <john.deriggi@gmail.com>
2020-09-16 09:34:53 -0700

    removes duplicated code

--- tides/src/main/java/com/slingshot/tides/WaterLevelController.java
+++ tides/src/main/java/com/slingshot/tides/WaterLevelController.java
@@ -1,5 +1,6 @@
 package com.slingshot.tides;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.List;
@@ -15,47 +16,54 @@ import org.springframework.web.bind.annotation.RestController;
 public class WaterLevelController {
     final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
-
     @RequestMapping(value = "/waterlevel", method = RequestMethod.GET)
-	public ResponseEntity<?> getWaterLevlels() throws Exception {
-	    return ResponseEntity.ok(DataLoader.getCacheTimestamp());
+    public ResponseEntity<?> getWaterLevlels() throws Exception {
+        return ResponseEntity.ok(DataLoader.getCacheTimestamp());
     }
 
     @RequestMapping(value = "/waterlevel/stats", method = RequestMethod.GET)
-	public ResponseEntity<?> getStats() throws Exception {
-	    return ResponseEntity.ok(DataLoader.getStats());
+    public ResponseEntity<?> getStats() throws Exception {
+        return ResponseEntity.ok(DataLoader.getStats());
     }
 
     @RequestMapping(value = "/waterlevel/{year}/{month}/{day}/{hh}/{mm}", method = RequestMethod.GET)
-	public ResponseEntity<?> getWaterLevelAtTime(@PathVariable Integer year, @PathVariable Integer month, @PathVariable Integer day, @PathVariable Integer hh, @PathVariable Integer mm) throws Exception {
-        
-        StringBuilder sb=  new StringBuilder();
-        sb.append(year).append("-").append(month).append("-")
-        .append(day).append(" ").append(" ").append(hh).append(":").append(mm);
-        
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        WaterLevel wl = new WaterLevel(sdf.parse(sb.toString()), null);
+    public ResponseEntity<?> getWaterLevelAtTime(@PathVariable Integer year, @PathVariable Integer month,
+            @PathVariable Integer day, @PathVariable Integer hh, @PathVariable Integer mm) throws Exception {
 
         List<WaterLevel> cache = DataLoader.getCacheTimestamp();
-        Integer index = Collections.binarySearch(cache, wl);
+        Integer index = Collections.binarySearch(cache, fromDate(year, month, day, hh, mm));
 
         return ResponseEntity.ok(cache.get(index));
     }
 
     @RequestMapping(value = "/waterlevel/{year}/{month}/{day}/{hh}/{mm}/{numDays}", method = RequestMethod.GET)
-	public ResponseEntity<?> getWaterLevelsRadius(@PathVariable Integer year, @PathVariable Integer month, @PathVariable Integer day, @PathVariable Integer hh, @PathVariable Integer mm, @PathVariable Integer numDays) throws Exception {
-        
-        StringBuilder sb=  new StringBuilder();
-        sb.append(year).append("-").append(month).append("-")
-        .append(day).append(" ").append(" ").append(hh).append(":").append(mm);
-        
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        WaterLevel wl = new WaterLevel(sdf.parse(sb.toString()), null);
+    public ResponseEntity<?> getWaterLevelsRadius(@PathVariable Integer year, @PathVariable Integer month,
+            @PathVariable Integer day, @PathVariable Integer hh, @PathVariable Integer mm,
+            @PathVariable Integer numDays) throws Exception {
+
+        WaterLevel wl = fromDate(year, month, day, hh, mm);
 
         List<WaterLevel> cache = DataLoader.getCacheTimestamp();
         Integer index = Collections.binarySearch(cache, wl);
-        List<WaterLevel> sublist = cache.subList(index, index + numDays > cache.size()-1? cache.size()-1 : index + numDays );
-    
+        List<WaterLevel> sublist = cache.subList(index,
+                index + numDays > cache.size() - 1 ? cache.size() - 1 : index + numDays);
+
         return ResponseEntity.ok(sublist);
+    }
+
+    private WaterLevel fromDate(Integer year, Integer month, Integer day, Integer hh, Integer mm) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(year).append("-").append(month).append("-").append(day).append(" ").append(" ").append(hh).append(":")
+                .append(mm);
+
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        WaterLevel wl;
+        try {
+            wl = new WaterLevel(sdf.parse(sb.toString()), null);
+        } catch (ParseException e) {
+            System.out.println("date parse error");
+            return null;
+        }
+        return wl;
     }
 }
